b1c2eed1db70c768ee088712d76614f3d15909f9
shannonu42 <84482811+shannonu42@users.noreply.github.com>
2026-03-23 19:02:30 +0000

    update KML URL to fetch new files not cached ones

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function initMap() {
     });
 
     const kmlLayer = new google.maps.KmlLayer({
-        url: "https://raw.githubusercontent.com/shannonu42/Underwood-Lab4-Google-Maps/main/letter_s.kml",
+        url: "https://raw.githubusercontent.com/shannonu42/Underwood-Lab4-Google-Maps/main/letter_s.kml?v=3",
         map: map,
         preserveViewport: true
     });
