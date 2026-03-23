b832145c5db5f911bc276a0f2452c37e5c2811f2
shannonu42 <84482811+shannonu42@users.noreply.github.com>
2026-03-23 18:22:06 +0000

    Troubleshooting

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ let map;
 let bikeLayer;
 let transitLayer;
 let trafficLayer;
-new google.maps.Marker({
-    position: { lat: 43.24841, lng: -81.82342 },
-    map: map,
-    title: "Pinery Provincial Park"
-});
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -17,23 +12,19 @@ function initMap() {
     const kmlLayer = new google.maps.KmlLayer({
         url: "https://raw.githubusercontent.com/shannonu42/Underwood-Lab4-Google-Maps/main/letter_s.kml",
         map: map,
-        preserveViewport: false
+        preserveViewport: true
     });
 
     bikeLayer = new google.maps.BicyclingLayer();
     transitLayer = new google.maps.TransitLayer();
     trafficLayer = new google.maps.TrafficLayer();
 }
-
 function toggleBicycle() {
     bikeLayer.setMap(bikeLayer.getMap() ? null : map);
 }
-
 function toggleTransit() {
     transitLayer.setMap(transitLayer.getMap() ? null : map);
 }
-
 function toggleTraffic() {
     trafficLayer.setMap(trafficLayer.getMap() ? null : map);
 }
-
