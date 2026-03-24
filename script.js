f8d3b79c041a8f5e49b4b97c7c95e4d2f1751258
shannonu42 <84482811+shannonu42@users.noreply.github.com>
2026-03-24 02:47:07 +0000

    Debugging

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/******************************************************
- * File: script.js
- * Author: Shannon Underwood
- * Date: March 22 2026
- *
- * Description:
- * This JavaScript file initializes the Google Map,
- * loads the KML polygon layer for the letter S,
- * and allows the user to toggle bicycle, transit,
- * and traffic layers on the map.
- ******************************************************/
-
 let map;
 let bikeLayer;
 let transitLayer;
@@ -19,17 +7,23 @@ let kmlLayer;
 const sCenter = { lat: 43.2837, lng: -81.7949 };
 
 function initMap() {
+    console.log("initMap started");
+
     map = new google.maps.Map(document.getElementById("map"), {
         center: sCenter,
         zoom: 13,
         mapTypeId: "roadmap"
     });
 
+    console.log("Map created");
+
     kmlLayer = new google.maps.KmlLayer({
         url: "https://raw.githubusercontent.com/shannonu42/Underwood-Lab4-Google-Maps/main/letter_s.kml?v=23",
         preserveViewport: true,
         map: map
     });
+
+    console.log("KML layer created");
 
     kmlLayer.addListener("status_changed", () => {
         console.log("KML status:", kmlLayer.getStatus());
@@ -38,41 +32,32 @@ function initMap() {
     bikeLayer = new google.maps.BicyclingLayer();
     transitLayer = new google.maps.TransitLayer();
     trafficLayer = new google.maps.TrafficLayer();
+
+    console.log("All layers ready");
 }
 
-// Clears all transportation layers before turning on another
+window.initMap = initMap;
+
 function clearLayers() {
     bikeLayer.setMap(null);
     transitLayer.setMap(null);
     trafficLayer.setMap(null);
 }
 
-// Toggles the bicycle layer
 function toggleBicycle() {
     const isOn = bikeLayer.getMap();
     clearLayers();
-
-    if (!isOn) {
-        bikeLayer.setMap(map);
-    }
+    if (!isOn) bikeLayer.setMap(map);
 }
 
-// Toggles the transit layer
 function toggleTransit() {
     const isOn = transitLayer.getMap();
     clearLayers();
-
-    if (!isOn) {
-        transitLayer.setMap(map);
-    }
+    if (!isOn) transitLayer.setMap(map);
 }
 
-// Toggles the traffic layer
 function toggleTraffic() {
     const isOn = trafficLayer.getMap();
     clearLayers();
-
-    if (!isOn) {
-        trafficLayer.setMap(map);
-    }
+    if (!isOn) trafficLayer.setMap(map);
 }
